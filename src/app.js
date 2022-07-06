a8cb05b7ecba23c2c37e415d7784522781c7f075
sngillard <108188646+sngillard@users.noreply.github.com>
2022-07-06 16:03:11 -0400

    showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-alert("Hello World");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "d39dbaacbf9289ab2454aa17a19d8eb5";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=boston&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
