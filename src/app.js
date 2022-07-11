f1618e18bd5897d019f1be5309d79c76707d394a
sngillard <108188646+sngillard@users.noreply.github.com>
2022-07-11 10:51:51 -0400

    Updated apiKey and apiUrl for getForecast function.

--- src/app.js
+++ src/app.js
@@ -55,8 +55,9 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "d39dbaacbf9289ab2454aa17a19d8eb5";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
   axios.get(apiUrl).then(displayForecast);
 }
 
