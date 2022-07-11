ca55aa072cc723ca0a6df4a196a53c80acb0a851
sngillard <108188646+sngillard@users.noreply.github.com>
2022-07-11 11:36:58 -0400

    Displayed forecast data using API.

--- src/app.js
+++ src/app.js
@@ -22,31 +22,48 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tues", "Wed"];
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
      <div class="col-2">
-       <div class="weather-forecast-date day">${day}</div>
+       <div class="weather-forecast-date day">${formatDay(forecastDay.dt)}</div>
        <img
-         src="http://openweathermap.org/img/wn/50d@2x.png"
+         src="http://openweathermap.org/img/wn/${
+           forecastDay.weather[0].icon
+         }@2x.png"
          alt=""
          width="38"
          class="weather-image"
        />
        <div class="weather-forecast-temperatures">
-         <span class="temperature-high">89&deg;</span>
-         <span class="temperature-low">59&deg;</span>
+         <span class="temperature-high"> ${Math.round(
+           forecastDay.temp.max
+         )}&deg; </span>
+         <span class="temperature-low"> ${Math.round(
+           forecastDay.temp.min
+         )}&deg; </span>
        </div>
      </div>
    `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
