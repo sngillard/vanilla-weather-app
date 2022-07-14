15f6771e4d8686ed936b96d04a99c5a997d67a4b
sngillard <108188646+sngillard@users.noreply.github.com>
2022-07-14 13:53:27 -0400

    Removed "currently" from description for better fit

--- src/app.js
+++ src/app.js
@@ -80,41 +80,6 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 
-//this function shows the current forecast at the top of the app (not the six day forecast)
-function displayTemperature(response) {
-  console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let feelsLikeElement = document.querySelector("#real-feel");
-  let todaysHighElement = document.querySelector("#todays-high");
-  let todaysLowElement = document.querySelector("#todays-low");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-
-  fahrenheitTemperature = response.data.main.temp;
-
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
-  todaysHighElement.innerHTML = Math.round(response.data.main.temp_max);
-  todaysLowElement.innerHTML = Math.round(response.data.main.temp_min);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForecast(response.data.coord);
-}
-
 //this function connnects the api information for a typed city
 function search(city) {
   let apiKey = "0bad4315e2bbb02795a47ba528cdb573";
@@ -135,7 +100,55 @@ function showPosition(position) {
   search(searchButtonElement.value);
 }
 
-navigator.geolocation.getCurrentPosition(showPosition);
+//this function is for the current location button
+
+function relativePosition(position) {
+  let apiKey = "0bad4315e2bbb02795a47ba528cdb573";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(relativePosition);
+}
+
+//this function shows the current forecast at the top of the app (not the six day forecast)
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let feelsLikeElement = document.querySelector("#real-feel");
+  let todaysHighElement = document.querySelector("#todays-high");
+  let todaysLowElement = document.querySelector("#todays-low");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  fahrenheitTemperature = response.data.main.temp;
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  todaysHighElement.innerHTML = Math.round(response.data.main.temp_max);
+  todaysLowElement.innerHTML = Math.round(response.data.main.temp_min);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
 
 //this function displays the current temperature in degrees celsius via C link
 function displayCelsiusTemperature(event) {
@@ -158,6 +171,9 @@ function displayFahrenheitTemperature(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
 
 let fahrenheitTemperature = null;
 
